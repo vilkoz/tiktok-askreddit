a7ec0aaafb9cab78ceccef93830722bf74d7f370
Vitalii Rybalko <vilko.a@gmail.com>
2022-07-11 15:19:41 +0300

    gen more b1

--- gen_words.py
+++ gen_words.py
@@ -22,8 +22,9 @@ def main():
     chunk_size = 3
     results = [results[i : i + chunk_size] for i in range(0, len(results), chunk_size)]
 
+    part_offset = 7
     for i, r in enumerate(results):
-        print({"title": f"{title} ч. {i}", "parts": r})
+        print({"title": f"{title} ч. {i+part_offset}", "parts": r})
     with open(argv[1], "w") as f:
         f.write("".join(lines))
 
